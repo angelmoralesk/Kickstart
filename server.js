5616079786d18d4ea5bd0ce3ab3fbdab5fdb1784
Angel Morales <a.jesse3001@gmail.com>
2018-05-01 15:35:39 -0500

    changes for heroku

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const app = next({
 });
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
+
+const port = process.env.PORT || 3000;
+
+
 app.prepare().then(() => {
-    createServer(handler).listen(3000, (err) => {
+    createServer(handler).listen(port, (err) => {
         if(err) {
           console.log('Hubo error !');
           throw err;
